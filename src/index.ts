061b703a01012b0e0e9930eb09e7aad3aa5de45d
Hexona <xuanjiap@lolicon.ac.cn>
2023-03-12 07:47:57 -0400

    feat: Bot Market online verification

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { apexConnect } from 'commands/apex/apex.connect.app';
 import { apexSearch } from 'commands/apex/apex.search.app';
+import auth from 'configs/auth';
 import { bot } from 'init/client';
 import { apexMenu } from './commands/apex/apex.menu';
 
@@ -33,3 +35,30 @@ bot.message.on('allTextMessages', (event) => {
 })
 
 bot.logger.debug('system init success');
+
+botMarketStayOnline();
+function botMarketStayOnline() {
+    axios({
+        url: 'http://bot.gekj.net/api/v1/online.bot',
+        method: "POST",
+        headers: {
+            uuid: auth.botMarketUUID
+        }
+    }).then((res) => {
+        if (res.data.code == 0) {
+            bot.logger.debug(`BotMarket: Successfully updated online status with remote returning: `);
+            bot.logger.debug(res.data);
+            setTimeout(botMarketStayOnline, (res.data.data.onTime + 5) * 1000);
+        } else if (res.data.code == -1) {
+            bot.logger.warn(`BotMarket: Failed updating online status with remote returning: `);
+            bot.logger.warn(res.data);
+            bot.logger.warn(`BotMarket: Retries in 30 minutes`);
+            setTimeout(botMarketStayOnline, 30 * 60 * 1000);
+        }
+    }).catch((e) => {
+        bot.logger.warn(`BotMarket: Failed updating online status with remote returning: `);
+        bot.logger.warn(e.message);
+        bot.logger.warn(`BotMarket: Retries in 30 minutes`);
+        setTimeout(botMarketStayOnline, 30 * 60 * 1000);
+    })
+}
